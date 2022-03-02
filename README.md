62b6e9fe8c4d7a2e5603f55d313e5e5c4555cf7e
Vinanddrinks Vodka <66210430+Vinanddrinks@users.noreply.github.com>
2022-03-02 15:26:37 +0100

    added description for setup in readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Volagal bot
  volagal discord bot
+
+# how to config
+
+the bot expect to find a config.json file answering to this syntax at the root of the package :
+
+{"BOT_PROPERTIES":{
+    "TOKEN": "Your Token Here",
+    "CONSOLE_INFO":"VOLABOT Info >",
+    "CONSOLE_WARN":"VOLABOT Warn >",
+    "CONSOLE_ERROR":"VOLABOT ERROR >"
+}}
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require('path')
 const { Intents } = DiscordJS
 const config = require("./config.json")
 const client = new DiscordJS.Client({
-  // These intents are recommended for the built in help menu
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
@@ -17,7 +16,6 @@ client.on('ready', () => {
   new WOKCommands(client, {
     // The name of the local folder for your command files
     commandsDir: path.join(__dirname, 'commands'),
-    testServers:['401366219803983882'],
   })
   .setDefaultPrefix('--')
   console.log(`${config.BOT_PROPERTIES.CONSOLE_INFO} Logged in Discord as ${client.user.tag}`)
